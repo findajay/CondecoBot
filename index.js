2b6fef3f22f3bb0d3fdd916bcea76aef1948c4ca
AJAY <ajay.20093@gmail.com>
2017-03-01 16:47:47 +0530

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const http = require('http');
+const http = require('http')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -29,9 +29,9 @@ app.get('/webhook/', function (req, res) {
 	}
 })
 
-  //  http.get('https://qa.condecodev.com', (res) => {
+    http.get('www.google.com', (res) => {
 
-   // });
+   });
 
 // to post data
 app.post('/webhook/', function (req, res) {
