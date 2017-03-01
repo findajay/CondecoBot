130db919effbf83e722e5cec9a43083c57019339
AJAY <ajay.20093@gmail.com>
2017-03-01 14:07:46 +0530

    Test checkin

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender, "Yo AJAY")
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
